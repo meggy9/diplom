7a88cdf5f79390b8e01fbec5891c892973b4908f
Ramazan Mirzoyev <meggy9@mail.ru>
2019-06-06 16:11:16 +0300

    Добавлен фильтр с инпутамии прочим говном

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var app = new Vue({
 	el: '#app',
-	data: {
-		organizationName: '',
-		organizationNumber: '',
-		dateOfEntry: '',
-		organizationAddress: ''
-	}
+	data: function() {
+		return {
+			organizationName: '',
+			organizationNumber: '',
+			dateOfEntry: '',
+			organizationAddress: ''
+		}
+	},
 	methods: {
 		getList: function() {
-			ajax({url: "list.php", success: function() {
+			$.ajax({url: "list.php", success: function() {
 				alert("Yo, guys!!!");
 			}})
 		}
