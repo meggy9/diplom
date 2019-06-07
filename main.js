965032c2d8e34bf01db5db41575c798e8771ba35
Ramazan <meggy9@mail.ru>
2019-06-07 07:15:19 +0300

    Добавил привязку полей к переменным

--- main.js
+++ main.js
@@ -3,16 +3,29 @@ var app = new Vue({
 	data: function() {
 		return {
 			organizationName: '',
-			organizationNumber: '',
-			dateOfEntry: '',
-			organizationAddress: ''
+			area: '',
+			organizationType: '',
+			serviceForm: [],
+			socialServicesType: [],
+			recipientCategory: [],		
+			ownershipType: []
 		}
 	},
 	methods: {
 		getList: function() {
-			$.ajax({url: "list.php", success: function() {
-				alert("Yo, guys!!!");
-			}})
-		}
+			$.ajax({
+		        url:     url, //url страницы (list.php)
+		        type:     "POST", //метод отправки
+		        dataType: "html", //формат данных
+		        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+		        success: function(response) { //Данные отправлены успешно
+		        	result = $.parseJSON(response);
+		        	$('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
+		    	},
+		    	error: function(response) { // Данные не отправлены
+		            $('#result_form').html('Ошибка. Данные не отправлены.');
+		    	}
+		 	})
+		}		
 	}
 })
